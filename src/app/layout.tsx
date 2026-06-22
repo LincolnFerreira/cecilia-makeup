33a44e468e255022b340f685b9fef86db940458e
Lincoln Ferreira <lincolnmedicinadireta@gmail.com>
2026-06-22 16:59:34 +0000

    A tree hydrated but some attributes of the server rendered HTML didn't m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Belleza&family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased selection:bg-primary/20">
+      <body className="font-body antialiased selection:bg-primary/20" suppressHydrationWarning>
         {children}
       </body>
     </html>
